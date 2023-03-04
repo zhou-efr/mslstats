f31a083ad0bd7f4ea20ae3c82c24c279a2d835c0
zhou-efr <killian.zhou@efrei.net>
2023-03-04 13:22:56 +0100

    fix get user

--- src/twitch/getUser.js
+++ src/twitch/getUser.js
@@ -23,11 +23,11 @@ export const getUser = async (username) => {
         const data = await res.json();
 
         if (res.status != 200) {
-            throw new Error(`getLatestStream - ${res.status} : ${JSON.stringify(data)}`);
+            throw new Error(`getUser - ${res.status} : ${JSON.stringify(data)}`);
         }
 
         return data.data[0];
     } catch (error) {
-        throw new Error(`getLatestStream : ${channel} - ${JSON.stringify(error)}`);
+        throw new Error(`getUser : ${username} - ${JSON.stringify(error)}`);
     }
 }
