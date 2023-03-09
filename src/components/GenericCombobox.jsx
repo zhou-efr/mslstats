8a4e0055838c5ff53722fcefe5566ae71e85dbbe
zhou-efr <killian.zhou@efrei.net>
2023-03-09 13:36:51 +0100

    fix data register

--- src/components/GenericCombobox.jsx
+++ src/components/GenericCombobox.jsx
@@ -42,7 +42,7 @@ export default function GenericCombobox({ genericarray, query, setQuery }) {
                 <Combobox.Input
                     className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     onChange={(event) => setQuery(event.target.value)}
-                    displayValue={(person) => person?.name}
+                    displayValue={query}
                 />
                 <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
                     <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -74,7 +74,7 @@ export default function GenericCombobox({ genericarray, query, setQuery }) {
                         >
                             {({ active, selected }) => (
                                 <>
-                                    <span className={classNames('block truncate', selected && 'font-semibold')}>{person.name}</span>
+                                    <span className={classNames('block truncate', selected && 'font-semibold')}>{person}</span>
 
                                     {selected && (
                                         <span
