bc8f6f250514a302b80288b9ab8ea77ff117619a
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2023-03-16 18:36:33 +0100

    add yt link

--- src/pages/games/index.jsx
+++ src/pages/games/index.jsx
@@ -32,13 +32,13 @@ export default function GamesPage({games}) {
                     className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
                 >
                     <div className="flex-shrink-0">
-                        <Image className="h-10 w-10 rounded-full" src={person.imageUrl} alt="" width={40} height={40}/>
+                        <Image className="h-10 w-10 rounded-full" src={person.thumbnail} alt="" width={40} height={40}/>
                     </div>
                     <div className="min-w-0 flex-1">
                         <Link href={"/games/" + person._id} className="focus:outline-none">
                             <span className="absolute inset-0" aria-hidden="true"/>
                             <p className="text-sm font-medium text-gray-900">{person.title}</p>
-                            <p className="truncate text-sm text-gray-500">{person.numberOfSessions} streams</p>
+                            <p className="truncate text-sm text-gray-500">{person.numberOfSessions} streams </p>
                         </Link>
                     </div>
                 </div>
