b49252a27baa820fbfbc4793bdb950fee8b2a098
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2023-03-19 23:42:04 +0100

    update game model

--- src/lib/mongoose/Game/GameModel.js
+++ src/lib/mongoose/Game/GameModel.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 
 const gameSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    expectedDuration: {type: Number, required: true},
-    totalDuration: {type: Number, required: true},
-    finished: {type: Boolean, required: true},
-    numberOfSessions: {type: Number, required: true},
-    thumbnail: {type: String, required: true},
-    youtube: {type: String, required: true},
+    title: { type: String, required: true },
+    expectedDuration: { type: Number, required: true },
+    totalDuration: { type: Number, required: true },
+    finished: { type: Boolean, required: true },
+    numberOfSessions: { type: Number, required: true },
+    thumbnail: { type: String, required: false },
+    youtube: { type: String, required: true },
 });
 
 module.exports = mongoose.models.Game || mongoose.model('Game', gameSchema);
