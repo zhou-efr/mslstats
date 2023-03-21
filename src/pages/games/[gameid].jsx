2567da934ef60ea9649d1866f92cc87967a6286d
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2023-03-21 17:22:38 +0100

    game_played

--- src/pages/games/[gameid].jsx
+++ src/pages/games/[gameid].jsx
@@ -1,19 +1,19 @@
-import {CheckCircleIcon, UserIcon} from '@heroicons/react/20/solid'
-import {getGames} from "@mongo/Game/getGames";
-import {getStreams} from "@mongo/Stream/getStreams";
+import { CheckCircleIcon, UserIcon } from '@heroicons/react/20/solid'
+import { getGames } from "@mongo/Game/getGames";
+import { getStreams } from "@mongo/Stream/getStreams";
 import Link from "next/link";
-import {getUser} from "@mongo/user/getUser";
-import {getSession} from "@auth0/nextjs-auth0";
-import {useRouter} from "next/navigation";
-import {useState} from "react";
-import {useUser} from "@auth0/nextjs-auth0/client";
+import { getUser } from "@mongo/user/getUser";
+import { getSession } from "@auth0/nextjs-auth0";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
 export async function getServerSideProps(ctx) {
-    const {gameid} = ctx.params;
+    const { gameid } = ctx.params;
     const session = await getSession(ctx.req, ctx.res);
     const rawstreams = await getStreams();
     const rawgames = await getGames();
@@ -33,8 +33,8 @@ export async function getServerSideProps(ctx) {
     }
 
     const streams = rawstreams.map((stream, index) => {
-        const {_doc} = stream;
-        if (_doc.game_planned === gamename || _doc.game_secondary === gamename)
+        const { _doc } = stream;
+        if (_doc.game_played === gamename || _doc.game_secondary === gamename)
             return {
                 ..._doc,
                 id: index + 1,
@@ -68,14 +68,14 @@ function YoutubeIcon(props) {
     return (
         <svg viewBox="0 0 256 256" {...props}>
             <path fillRule="evenodd" clipRule="evenodd"
-                  d="M227.707 44.3638C238.735 47.3133 247.337 55.9487 250.319 66.9765C258.119 98.4376 257.611 157.919 250.483 189.871C247.534 200.899 238.898 209.502 227.871 212.484C196.737 220.186 57.2924 219.235 27.9614 212.484C16.9337 209.535 8.33102 200.899 5.34877 189.871C-2.00854 159.885 -1.50058 96.4713 5.18491 67.1404C8.13439 56.1126 16.7698 47.5099 27.7976 44.5277C69.418 35.8431 212.894 38.6451 227.707 44.3638ZM103.174 90.0798L170.029 128.423L103.174 166.766V90.0798Z"/>
+                d="M227.707 44.3638C238.735 47.3133 247.337 55.9487 250.319 66.9765C258.119 98.4376 257.611 157.919 250.483 189.871C247.534 200.899 238.898 209.502 227.871 212.484C196.737 220.186 57.2924 219.235 27.9614 212.484C16.9337 209.535 8.33102 200.899 5.34877 189.871C-2.00854 159.885 -1.50058 96.4713 5.18491 67.1404C8.13439 56.1126 16.7698 47.5099 27.7976 44.5277C69.418 35.8431 212.894 38.6451 227.707 44.3638ZM103.174 90.0798L170.029 128.423L103.174 166.766V90.0798Z" />
         </svg>
     )
 }
 
-export default function GamePage({streams, game}) {
+export default function GamePage({ streams, game }) {
     const [gameStreams, setGameStreams] = useState(streams);
-    const {user, error, isLoading} = useUser();
+    const { user, error, isLoading } = useUser();
 
     const router = useRouter();
 
@@ -135,11 +135,11 @@ export default function GamePage({streams, game}) {
                                     height={200}
                                     patternUnits="userSpaceOnUse"
                                 >
-                                    <path d="M0.5 0V200M200 0.5L0 0.499983"/>
+                                    <path d="M0.5 0V200M200 0.5L0 0.499983" />
                                 </pattern>
                             </defs>
                             <rect width="100%" height="100%" strokeWidth={0}
-                                  fill="url(#e87443c8-56e4-4c20-9111-55b82fa704e3)"/>
+                                fill="url(#e87443c8-56e4-4c20-9111-55b82fa704e3)" />
                         </svg>
                         <figure className="border-l border-indigo-600 pl-8">
                             <p className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Tous les lives
@@ -163,9 +163,9 @@ export default function GamePage({streams, game}) {
                                         - {durationToTime(game.totalDuration)} de stream
                                     </div>
                                     <Link href={game.youtube || "https://www.youtube.com/@MathieuSommetLive"}
-                                          target={"_blank"}
-                                          className={"fill-slate-400 group-hover:fill-slate-600 text-slate-400 hover:text-slate-600"}><YoutubeIcon
-                                        className="inline w-4 h-4"/> Voir sur YouTube</Link>
+                                        target={"_blank"}
+                                        className={"fill-slate-400 group-hover:fill-slate-600 text-slate-400 hover:text-slate-600"}><YoutubeIcon
+                                            className="inline w-4 h-4" /> Voir sur YouTube</Link>
                                 </div>
                             </figcaption>
                         </figure>
@@ -179,7 +179,7 @@ export default function GamePage({streams, game}) {
                                         <div className="relative pb-8">
                                             {eventIdx !== streams.length - 1 ? (
                                                 <span className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
-                                                      aria-hidden="true"/>
+                                                    aria-hidden="true" />
                                             ) : null}
                                             <div className="relative flex space-x-3">
                                                 <div>
