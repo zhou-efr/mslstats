34ef058b01b541775447634c7b017f357bc8b712
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2023-03-31 11:33:59 +0200

    disclaimer

--- src/pages/disclaimer.jsx
+++ src/pages/disclaimer.jsx
@@ -1,5 +1,5 @@
 import { Container } from '@/components/Container'
-import { CheckCircleIcon, InformationCircleIcon } from '@heroicons/react/20/solid'
+import { CheckCircleIcon, InformationCircleIcon, ShieldCheckIcon } from '@heroicons/react/20/solid'
 
 export default function Example() {
     return (
@@ -26,8 +26,8 @@ export default function Example() {
                         </p>
                         <p className='my-3'>
                             Pour parvenir à cela, l{"'"}authentification est gérée par un plan gratuit Auth0
-                            (<a className='text-indigo-500 hover:text-indigo-700 active:text-pink-900' href="https://www.okta.com/privacy-policy/">lien RGPD</a>). Est ensuite sauvegardé dans une base de données
-                            MongoDB Atlas (<a className='text-indigo-500 hover:text-indigo-700 active:text-pink-900' href="https://www.mongodb.com/legal/privacy-policy">lien RGPD</a>) la liste des streamers suivis au même titre
+                            (<a rel="noreferrer" target={"_blank"} className='text-indigo-500 hover:text-indigo-700 active:text-pink-900' href="https://www.okta.com/privacy-policy/">lien RGPD</a>). Est ensuite sauvegardé dans une base de données
+                            MongoDB Atlas (<a rel="noreferrer" target={"_blank"} className='text-indigo-500 hover:text-indigo-700 active:text-pink-900' href="https://www.mongodb.com/legal/privacy-policy">lien RGPD</a>) la liste des streamers suivis au même titre
                             que l{"'"}email fourni à l{"'"}inscription.
                         </p>
                         <ul role="list" className="mt-8 max-w-xl space-y-8 text-gray-600">
@@ -36,7 +36,7 @@ export default function Example() {
                                 <span>
                                     <strong className="font-semibold text-gray-900">La liste des streamers</strong> suivis par un utilisateur est
                                     utilisée pour récupérer la liste des streams à afficher sur la page d{"'"}accueil. Cette liste est directement
-                                    récupérée auprès de Twitch via son API (<a className='text-indigo-500 hover:text-indigo-700 active:text-pink-900' href="https://www.twitch.tv/p/en/legal/privacy-notice/">lien RGPD</a>).
+                                    récupérée auprès de Twitch via son API (<a rel="noreferrer" target={"_blank"} className='text-indigo-500 hover:text-indigo-700 active:text-pink-900' href="https://www.twitch.tv/p/en/legal/privacy-notice/">lien RGPD</a>).
                                     La liste des streamers est fournie par l{"'"}utilisateur depuis l{"'"}onglet Paramètres.
                                 </span>
                             </li>
@@ -57,7 +57,54 @@ export default function Example() {
                         </p>
                         <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">Au sujet de la sécurité</h2>
                         <p className="mt-6">
-                            WIP
+                            D'expérience, la meilleure manière d'avoir un site sûr et des données en sécurité est de laisser les autres faire.
+                            Ainsi, vous pourrez retrouver dans les notices de confidentialité des SaaS que j'utilise une multitude de sécurités.
+                            Pour résumer, ils utilisent les standards en termes de chiffrement des données au repos et en transit ainsi que de
+                            sécurité physique et logiciel.
+                        </p>
+                        <ul role="list" className="mt-8 max-w-xl space-y-8 text-gray-600">
+                            <li className="flex gap-x-3">
+                                <ShieldCheckIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+                                <span>
+                                    <strong className="font-semibold text-gray-900">Vercel</strong> <a rel="noreferrer" target={"_blank"} className='text-indigo-500 hover:text-indigo-700 active:text-pink-900' href="https://vercel.com/security">Security notice</a>
+                                </span>
+                            </li>
+                            <li className="flex gap-x-3">
+                                <ShieldCheckIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+                                <span>
+                                    <strong className="font-semibold text-gray-900">Mongo DB Atlas</strong> <a rel="noreferrer" target={"_blank"} className='text-indigo-500 hover:text-indigo-700 active:text-pink-900' href="https://www.mongodb.com/cloud/atlas/security">Security notice</a>
+                                </span>
+                            </li>
+                            <li className="flex gap-x-3">
+                                <ShieldCheckIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+                                <span>
+                                    <strong className="font-semibold text-gray-900">Auth 0</strong> <a rel="noreferrer" target={"_blank"} className='text-indigo-500 hover:text-indigo-700 active:text-pink-900' href="https://auth0.com/security">Security, Privacy & Compliance</a>
+                                </span>
+                            </li>
+                            <li className="flex gap-x-3">
+                                <ShieldCheckIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+                                <span>
+                                    <strong className="font-semibold text-gray-900">Twitch</strong> <a rel="noreferrer" target={"_blank"} className='text-indigo-500 hover:text-indigo-700 active:text-pink-900' href="https://www.twitch.tv/jobs/fr-fr/legal/privacy-notice/">Privacy notice</a>
+                                </span>
+                            </li>
+                        </ul>
+                        <p className="mt-6">
+                            De mon côté, j'ai prêté attention aux recommandations de l'ANSSI, aux mots de passe des comptes de services, ainsi
+                            qu'à ce que tout accès en écriture aux bases de données soit contrôlé (accès administrateur uniquement ou accès
+                            limité). Cela peut sembler très fancy dit comme ça, mais en réalité, c'est très basique (et suffisant).
+                        </p>
+
+                        <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">Pour conclure</h2>
+                        <p className="mt-6">
+                            En somme, je ne conserve rien et n'utilise rien pour autre chose que ce à quoi sert l'application. En outre, j'essaie
+                            de faire les choses correctement autant pour éviter les problèmes que pour ne pas en avoir moi-même.
+                        </p>
+                        <p className="mt-6">
+                            Pour plus de questions, de demandes de suppression, etc., pinguez-moi sur Discord ou envoyez un e-mail à zhouefr@gmail.com.
+                        </p>
+                        <p className="mt-6">
+                            Bien à vous,<br />
+                            Un humble panda.
                         </p>
                     </div>
                 </div>
