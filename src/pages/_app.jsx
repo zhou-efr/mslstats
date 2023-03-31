00cdc20494de405c8086e46a4cf87f512270d6d9
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2023-03-31 14:02:20 +0200

    add suspense because why not

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,14 @@
-import {Layout} from '@/components/Layout'
+import { Layout } from '@/components/Layout'
 import Head from 'next/head'
 
-import {UserProvider} from '@auth0/nextjs-auth0/client';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
-import {Loading} from '@/components/Loading';
-import {usePageLoading} from '@/hook/usePageLoading';
-import {Analytics} from "@vercel/analytics/react";
+import { Loading } from '@/components/Loading';
+import { usePageLoading } from '@/hook/usePageLoading';
+import { Analytics } from "@vercel/analytics/react";
+import { Suspense } from 'react';
 
 export default function App({ Component, pageProps }) {
   const { isPageLoading } = usePageLoading();
@@ -19,18 +20,20 @@ export default function App({ Component, pageProps }) {
             MSL
           </title>
           <meta
-              name="description"
-              content="Conversations with the most tragically misunderstood people of our time."
+            name="description"
+            content="Conversations with the most tragically misunderstood people of our time."
           />
         </Head>
-        {
-          isPageLoading ?
-              <Loading/>
+        <Suspense fallback={<Loading />}>
+          {
+            isPageLoading ?
+              <Loading />
               :
               <Component {...pageProps} />
-        }
+          }
+        </Suspense>
       </Layout>
-      <Analytics/>
+      <Analytics />
     </UserProvider>
   )
 }
