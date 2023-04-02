e43f1297ed5e91e184101577815d82cb4319bf73
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2023-04-02 20:09:25 +0200

    metadatas

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,39 +1,39 @@
-import { Layout } from '@/components/Layout'
+import {Layout} from '@/components/Layout'
 import Head from 'next/head'
 
-import { UserProvider } from '@auth0/nextjs-auth0/client';
+import {UserProvider} from '@auth0/nextjs-auth0/client';
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
-import { Loading } from '@/components/Loading';
-import { usePageLoading } from '@/hook/usePageLoading';
-import { Analytics } from "@vercel/analytics/react";
-import { Suspense } from 'react';
+import {Loading} from '@/components/Loading';
+import {usePageLoading} from '@/hook/usePageLoading';
+import {Analytics} from "@vercel/analytics/react";
+import {Suspense} from 'react';
 
-export default function App({ Component, pageProps }) {
-  const { isPageLoading } = usePageLoading();
-  return (
-    <UserProvider>
-      <Layout>
-        <Head>
-          <title>
-            MSL
-          </title>
-          <meta
-            name="description"
-            content="Conversations with the most tragically misunderstood people of our time."
-          />
-        </Head>
-        <Suspense fallback={<Loading />}>
-          {
-            isPageLoading ?
-              <Loading />
-              :
-              <Component {...pageProps} />
-          }
-        </Suspense>
-      </Layout>
-      <Analytics />
-    </UserProvider>
-  )
+export default function App({Component, pageProps}) {
+    const {isPageLoading} = usePageLoading();
+    return (
+        <UserProvider>
+            <Layout>
+                <Head>
+                    <title>
+                        MSL
+                    </title>
+                    <meta
+                        name="description"
+                        content="Viewer de replay, c'est en regardant ma pile de side-projects non fini que j'en suis venu à réaliser une interface coolos pour rechercher facilement des replays de streams 🔍."
+                    />
+                </Head>
+                <Suspense fallback={<Loading/>}>
+                    {
+                        isPageLoading ?
+                            <Loading/>
+                            :
+                            <Component {...pageProps} />
+                    }
+                </Suspense>
+            </Layout>
+            <Analytics/>
+        </UserProvider>
+    )
 }
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,24 +1,36 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import {Head, Html, Main, NextScript} from 'next/document'
 
 export default function Document() {
-  return (
-    <Html className="bg-white antialiased" lang="en">
-      <Head>
-        <link
-          rel="preconnect"
-          href="https://cdn.fontshare.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          href="https://api.fontshare.com/v2/css?f[]=satoshi@700,500,400&display=swap"
-        />
-        <link rel="manifest" href="/manifest.webmanifest" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html className="bg-white antialiased" lang="en">
+            <Head>
+                <link
+                    rel="preconnect"
+                    href="https://cdn.fontshare.com"
+                    crossOrigin="anonymous"
+                />
+                <link
+                    rel="stylesheet"
+                    href="https://api.fontshare.com/v2/css?f[]=satoshi@700,500,400&display=swap"
+                />
+                <link rel="manifest" href="/manifest.webmanifest"/>
+
+
+                <meta property="og:title" content="MSL Stats"/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://mslstats.vercel.app/"/>
+                <meta property="og:image"
+                      content="https://github.com/zhou-efr/CDN/blob/main/mslstats/images/logotest3.png"/>
+                <meta property="og:description"
+                      content="Viewer de replay, c'est en regardant ma pile de side-projects non fini que j'en suis venu à réaliser une interface coolos pour rechercher facilement des replays de streams 🔍."/>
+                <meta name="theme-color" content="#3A0C87"/>
+
+                <meta name="twitter:card" content="summary_large_image"/>
+            </Head>
+            <body>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    )
 }
