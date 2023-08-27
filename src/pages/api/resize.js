e0a358a302d4be1043a3b9031554a2a0918252a6
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2023-08-27 16:55:23 +0200

    resize api

--- src/pages/api/resize.js
+++ src/pages/api/resize.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     const { width, height } = req.query;
-    const { url, imageUrl } = req.query;
-    
+    let { url, imageUrl } = req.query;
+
     if (imageUrl && !url) {
         url = imageUrl;
     }
