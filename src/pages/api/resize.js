dc71b85035845a574a432f68551d1e7fca55019c
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2023-08-27 16:54:13 +0200

    resize api

---
+++ src/pages/api/resize.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+    const { width, height } = req.query;
+    const { url, imageUrl } = req.query;
+    
+    if (imageUrl && !url) {
+        url = imageUrl;
+    }
+    
+    if (width && height && url) {
+        const response = await fetch(url);
+        const buffer = await response.arrayBuffer();
+        const sharp = require("sharp");
+        const resized = await sharp(buffer)
+        .resize(parseInt(width), parseInt(height))
+        .toBuffer();
+        res.setHeader("Content-Type", `image/jpg`);
+        res.setHeader("Cache-Control", `public, max-age=31536000, immutable`);
+        res.end(resized);
+    } else {
+        res.statusCode = 400;
+        res.end("Bad Request");
+    }
+}
